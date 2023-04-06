35c6f4124e6018dba9a54c0c563d0950df074f33
김철우 <kimchul8747@daum.net>
2023-04-06 21:45:33 +0900

    FIX :: 폴더명 수정

--- React/React clone-coding/youtube/src/App.js
+++ React/React clone-coding/youtube/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./component/header";
-import Body from "./component/Body";
+import Body from "./component/body";
 
 function App() {
   return (
--- React/React clone-coding/youtube/src/component/Body/index.js
+++ React/React clone-coding/youtube/src/component/Body/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Nav from "../Nav";
+import Nav from "../nav";
 import Bar from "../bar";
 import Content from "../content";
 
